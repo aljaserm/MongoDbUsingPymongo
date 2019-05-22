3ba62313ff0fb71a87b243134b67038056ece5c8
Mohammed alJaser <aljaserm7@gmail.com>
2019-05-21 22:07:39 -0400

    uncommented some code

--- DateTimeAndKeywords.py
+++ DateTimeAndKeywords.py
@@ -33,9 +33,9 @@ db = client.pyMongoTest
 user = db.users
 currentDate = datetime.datetime.now()
 oldDate = datetime.datetime(2009, 8, 11)
-# user1 = {"username": "MJ", "password": "MyPassword", "favoriteColor": "Blue", "FavoriteNumber": 7,
-#          "Hobbies": ["Coding", "TV", "Gaming"], "date": currentDate}
-# userId = user.insert_one(user1).inserted_id
+user1 = {"username": "MJ", "password": "MyPassword", "favoriteColor": "Blue", "FavoriteNumber": 7,
+         "Hobbies": ["Coding", "TV", "Gaming"], "date": currentDate}
+userId = user.insert_one(user1).inserted_id
 print(user.count_documents({"date": {"$gte": oldDate}}))
 print(user.count_documents({"username": {"$ne": "MJ"}}))
 print(user.count_documents({"username": {"$eq": "MJ"}}))
